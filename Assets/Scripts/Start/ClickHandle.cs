4bf0dbd2da710c582231466346bb835058ef1ea4
Yann001 <1156203992@qq.com>
2018-08-31 19:02:13 +0800

    init project

---
+++ Assets/Scripts/Start/ClickHandle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickHandle : MonoBehaviour {
+
+    private void Click()
+    {
+        SceneManager.LoadScene("Introduction");
+    }
+
+	// Use this for initialization
+	void Start () {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
